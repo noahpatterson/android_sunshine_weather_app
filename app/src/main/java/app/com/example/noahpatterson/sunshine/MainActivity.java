8244dd0b7be20c9bed4fcc7c142a8ee2726cc2c5
noahpatterson <n@noahpatterson.me>
2015-06-26 20:19:22 -0400

    Add settings activity and tie menu click to the Activity via intents

--- app/src/main/java/app/com/example/noahpatterson/sunshine/MainActivity.java
+++ app/src/main/java/app/com/example/noahpatterson/sunshine/MainActivity.java
@@ -1,7 +1,8 @@
 package app.com.example.noahpatterson.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -30,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+//            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+//            startActivity(settingsIntent);
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
