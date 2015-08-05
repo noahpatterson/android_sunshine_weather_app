6c97f8b2944f62f37b594c8c6db2e9768e8ec43b
noahpatterson <n@noahpatterson.me>
2015-08-05 07:13:42 -0400

    add sunshine theme

--- app/src/main/java/app/com/example/noahpatterson/sunshine/MainActivity.java
+++ app/src/main/java/app/com/example/noahpatterson/sunshine/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
+            getSupportActionBar().setElevation(0f);
         }
     }
 
