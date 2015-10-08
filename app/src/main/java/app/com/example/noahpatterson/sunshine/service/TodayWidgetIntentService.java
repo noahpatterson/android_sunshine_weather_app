5b6d3d7d0980d8a5825341457fab1e7756a57241
noahpatterson <n@noahpatterson.me>
2015-10-08 07:29:52 -0400

    WIP different sized widgets

--- app/src/main/java/app/com/example/noahpatterson/sunshine/service/TodayWidgetIntentService.java
+++ app/src/main/java/app/com/example/noahpatterson/sunshine/service/TodayWidgetIntentService.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
 import app.com.example.noahpatterson.sunshine.MainActivity;
@@ -28,12 +29,15 @@ public class TodayWidgetIntentService extends IntentService {
     private static final String[] FORECAST_COLUMNS = {
             WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
             WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
-            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
     };
     // these indices must match the projection
     private static final int INDEX_WEATHER_ID = 0;
     private static final int INDEX_SHORT_DESC = 1;
     private static final int INDEX_MAX_TEMP = 2;
+    private static final int INDEX_MIN_TEM = 3;
+    private  AppWidgetManager mAppWidgetManager;
 
     public TodayWidgetIntentService() {
         super("TodayWidgetIntentService");
@@ -42,8 +46,8 @@ public class TodayWidgetIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         // Retrieve all of the Today widget ids: these are the widgets we need to update
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this,
+        mAppWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = mAppWidgetManager.getAppWidgetIds(new ComponentName(this,
                 TodayWidgetProvider.class));
 
         // Get today's data from the ContentProvider
@@ -65,7 +69,9 @@ public class TodayWidgetIntentService extends IntentService {
         int weatherArtResourceId = Utility.getArtResourceForWeatherCondition(weatherId);
         String description = data.getString(INDEX_SHORT_DESC);
         double maxTemp = data.getDouble(INDEX_MAX_TEMP);
+        double minTemp = data.getDouble(INDEX_MIN_TEM);
         String formattedMaxTemperature = Utility.formatTemperature(this, maxTemp, false);
+        String formattedMinTemperature = Utility.formatTemperature(this, minTemp, false);
         data.close();
 
         // Perform this loop procedure for each Today widget
@@ -87,12 +93,25 @@ public class TodayWidgetIntentService extends IntentService {
             views.setOnClickPendingIntent(R.id.widget, pendingIntent);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            mAppWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
     private void setRemoteContentDescription(RemoteViews views, String description) {
         views.setContentDescription(R.id.widget_icon, description);
+    }
+
+    private int getWidgetMinWidth(int appWidgetId) {
+        Bundle options = mAppWidgetManager.getAppWidgetOptions(appWidgetId);
+        return options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+    }
+
+    private int selectWidgetLayout(int appWidgetId) {
+        int widgetMinWidth = getWidgetMinWidth(appWidgetId);
+        if (widgetMinWidth < 110) {
+            return 0;
+        }
+        return 0;
     }
 }
